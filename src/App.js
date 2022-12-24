177b98b2aadab6e2fa5261cd7e3a4168f04a63d6
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-12-25 02:12:25 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { DeleteIcon } from '@chakra-ui/icons';
+
+import {
+  Button,
+  Heading,
+  HStack,
+  IconButton,
+  Input,
+  Spacer,
+  VStack,
+  useToast,
+  Card,
+} from '@chakra-ui/react';
+import { nanoid } from 'nanoid';
+import axios from 'axios';
+
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [task, setTask] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (e) => {
+    e.preventDefault();
+
+    const newTodoObj = {
+      id: nanoid(),
+      task: task,
+    };
+
+    setTodos((prev) => [...prev, newTodoObj]);
+    setTask('');
+  };
+
+  const handleDelete = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VStack>
+      <Heading>Todo</Heading>
+      <Spacer />
+
+      <form onSubmit={(e) => addTodo(e)}>
+        <HStack>
+          <Input
+            placeholder='enter todo'
+            onChange={(e) => setTask(e.target.value)}
+            value={task}
+          />
+          <Button type='submit'>Add</Button>
+        </HStack>
+      </form>
+      <VStack width='20vw'>
+        {/* {todos.map(({ task, id }) => (
+          <IndividualTodo task={task} key={id} />
+        ))} */}
+
+        {todos.map((todo) => (
+          <Card direction='column' width='full' px='4' py='2' key={todo.id}>
+            <HStack justify='space-between' width='full'>
+              <span>{todo.task}</span>
+              <IconButton
+                icon={<DeleteIcon />}
+                isRound={true}
+                colorScheme='red'
+                onClick={() => handleDelete(todo.id)}
+              />
+            </HStack>
+          </Card>
+        ))}
+      </VStack>
+    </VStack>
   );
 }
 
